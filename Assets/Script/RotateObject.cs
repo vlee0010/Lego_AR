55681319da2eb39c207a4849857945cdcfa64c01
vlee0010 <vlee0010@student.monash.edu>
2020-11-23 22:46:45 +0700

    first commit

---
+++ Assets/Script/RotateObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+	public GameObject objectRotate;
+
+	public float rotateSpeed = 50f;
+	bool rotateStatus = false;
+
+	public void Rotasi()
+	{
+
+		if (rotateStatus == false)
+		{
+			rotateStatus = true;
+		}  
+		else
+		{
+			rotateStatus = false;
+		}
+	}
+
+	void Update()
+	{
+		if (rotateStatus == true)
+		{
+			objectRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+		}
+	}
+
+	
+}
